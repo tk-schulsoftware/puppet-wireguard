5731aa2c4cc7a26bb6deedd8bd20171807d5b812
Tobias Knipping <knipping@tk-schulsoftware.de>
2021-04-03 20:37:08 +0200

    add fact for pub-key

--- lib/facter/wireguard.rb
+++ lib/facter/wireguard.rb
@@ -1,6 +1,8 @@
 if File.file?('/etc/wireguard/wg0.pub')
   wireguard = {}
-  wireguard['wg0'] = File.read('/etc/wireguard/wg0.pub')
+  wireguard['wg0']['public_key'] = File.read('/etc/wireguard/wg0.pub')
+  wireguard['wg0']['endpoint'] = Facter.value(:fqdn) + ':8080'
+
   Facter.add('wireguard') do
     setcode do
       wireguard
