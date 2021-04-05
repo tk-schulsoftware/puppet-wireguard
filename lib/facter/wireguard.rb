4be1b746fc524adc8081ab35dc294409e3111947
Tobias Knipping <knipping@tk-schulsoftware.de>
2021-04-05 13:52:31 +0200

    modify wireguard fact

--- lib/facter/wireguard.rb
+++ lib/facter/wireguard.rb
@@ -1,20 +1,15 @@
 wireguard = {}
-port = 'port' # default to "port"
 
 Dir.glob('/etc/wireguard/*.conf') do |filename|
 
   interface = File.basename(filename).gsub('.conf', '')
-  File.open(filename).each do |line|
-    if line.match( /ListenPort = \d*/ )
-      port = line.gsub('ListenPort = ', '')
-    end
-  end
   private_key_path = '/etc/wireguard/' + interface + '.key'
 
-  wireguard[interface] = {}
-
   if File.file?(private_key_path)
+    wireguard[interface] = {}
+
     public_key = Puppet::Util::Execution.execute(['/usr/bin/wg', 'pubkey'], stdinfile: private_key_path)
+    port = Puppet::Util::Execution.execute(['/usr/bin/wg', 'show', interface, 'listen-port'])
     wireguard[interface]['public_key'] = public_key
     wireguard[interface]['endpoint'] = Facter.value(:fqdn) + ':' + port
   end
