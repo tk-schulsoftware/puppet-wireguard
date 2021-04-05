22a5d339c30abc465d0d18d50d19313b85f2bd8c
Tobias Knipping <knipping@tk-schulsoftware.de>
2021-04-05 12:14:21 +0200

    add fact for peers: public-key and endpoint

--- lib/facter/wireguard.rb
+++ lib/facter/wireguard.rb
@@ -1,11 +1,27 @@
-if File.file?('/etc/wireguard/wg0.pub')
-  wireguard = {}
-  wireguard['wg0']['public_key'] = File.read('/etc/wireguard/wg0.pub')
-  wireguard['wg0']['endpoint'] = Facter.value(:fqdn) + ':8080'
+wireguard = {}
+port = 'port' # default to "port"
 
-  Facter.add('wireguard') do
-    setcode do
-      wireguard
+Dir.glob('/etc/wireguard/*.conf') do |filename|
+
+  interface = File.basename(filename).gsub('.conf', '')
+  File.open(filename).each do |line|
+    if line.match( /ListenPort = \d*/ )
+      port = line.gsub('ListenPort = ', '')
     end
+  end
+  private_key_path = '/etc/wireguard/' + interface + '.key'
+
+  wireguard[interface] = {}
+
+  if File.file?(private_key_path)
+    public_key = Puppet::Util::Execution.execute(['/usr/bin/wg', 'pubkey'], stdinfile: private_key_path)
+    wireguard[interface]['public_key'] = public_key
+    wireguard[interface]['endpoint'] = Facter.value(:fqdn) + ':' + port
+  end
+end
+
+Facter.add('wireguard') do
+  setcode do
+    wireguard
   end
 end
