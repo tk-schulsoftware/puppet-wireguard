14b736a234920fbe598a4bc50b017b1d8d383e73
Tobias Knipping <knipping@tk-schulsoftware.de>
2021-04-05 14:37:03 +0200

    modify wireguard fact

--- lib/facter/wireguard.rb
+++ lib/facter/wireguard.rb
@@ -10,10 +10,10 @@ Dir.glob('/etc/wireguard/*.conf') do |filename|
 
     public_key = Puppet::Util::Execution.execute(['/usr/bin/wg', 'pubkey'], stdinfile: private_key_path)
     port = Puppet::Util::Execution.execute(['/usr/bin/wg', 'show', interface, 'listen-port']) || '51820'
-    local_ip = Facter.value(':ipaddress_' + interface) || '0.0.0.0'
+    local_ip = Facter.value(:networking)['interfaces'][interface]['ip'] || '0.0.0.0'
 
     wireguard[interface]['public_key'] = public_key
-    wireguard[interface]['endpoint'] = Facter.value(':fqdn') + ':' + port
+    wireguard[interface]['endpoint'] = Facter.value(:fqdn) + ':' + port
     wireguard[interface]['local_ip'] =  local_ip + '/32'
   end
 end
