1a51c51a110bad61ef71560fe971f8209bb80fd7
Aaron Lewandowski <a.lewandowski@tks.eu>
2024-03-12 12:25:57 +0100

    fix: for mac

--- lib/puppet/functions/wireguard/genpublickey.rb
+++ lib/puppet/functions/wireguard/genpublickey.rb
@@ -25,7 +25,16 @@ Puppet::Functions.create_function(:'wireguard::genpublickey') do
         File.write(public_key_path, public_key)
       end
     else
-      public_key = ''
+      if File.exist?('/usr/local/bin/wg')
+        if File.exist?(public_key_path)
+          public_key = File.read(public_key_path).strip
+        else
+          public_key = Puppet::Util::Execution.execute(['/usr/local/bin/wg', 'pubkey'], stdinfile: private_key_path)
+          File.write(public_key_path, public_key)
+        end
+      else
+        public_key = ''
+      end
     end
 
     public_key
